1d4f659e7f9a6e51780b7ef1714321a8e8f0d9ff
TheAdamBomb98 <adameliasford@gmail.com>
2018-03-24 12:49:13 -0500

    Rook possible moves Board constr. extra

--- src/Board.java
+++ src/Board.java
@@ -3,7 +3,6 @@ public class Board {
 	private Piece[][] board;
 	
 	public Board() {
-		
 		int i, j;
 		board = new Piece[8][8];
 		
@@ -12,39 +11,42 @@ public class Board {
 			for (j = 0; j < board[0].length; j++)
 			{
 				if (i == 1)
-					board[i][j] = new Pawn(i, j, false);
+					board[i][j] = new Pawn(i, j, false, "");
 				if (i == 6)
-					board[i][j] = new Pawn(i, j, true);
+					board[i][j] = new Pawn(i, j, true, "");
 				if (i == 0)
 				{
 					if (j == 0 || j == 7)
-						board[i][j] = new Rook(i, j, false);
+						board[i][j] = new Rook(i, j, false, "");
 					if (j == 1 || j == 6)
-						board[i][j] = new Knight(i, j, false);
+						board[i][j] = new Knight(i, j, false, "");
 					if (j == 2 || j == 5)
-						board[i][j] = new Bishop(i, j, false);
+						board[i][j] = new Bishop(i, j, false, "");
 					if (j == 3)
-						board[i][j] = new Queen(i, j, false);
+						board[i][j] = new Queen(i, j, false, "");
 					if (j == 4)
-						board[i][j] = new King(i, j, false);
+						board[i][j] = new King(i, j, false, "");
 				}
 				if (i == 7)
 				{
 					if (j == 0 || j == 7)
-						board[i][j] = new Rook(i, j, true);
+						board[i][j] = new Rook(i, j, true, "");
 					if (j == 1 || j == 6)
-						board[i][j] = new Knight(i, j, true);
+						board[i][j] = new Knight(i, j, true, "");
 					if (j == 2 || j == 5)
-						board[i][j] = new Bishop(i, j, true);
+						board[i][j] = new Bishop(i, j, true, "");
 					if (j == 3)
-						board[i][j] = new Queen(i, j, true);
+						board[i][j] = new Queen(i, j, true, "");
 					if (j == 4)
-						board[i][j] = new King(i, j, true);
+						board[i][j] = new King(i, j, true, "");
 				}
 			}
 		}
 	}
 	
+	public Board(Piece[][] b) {
+		board = b;
+	}
 	public void displayBoard(Board b)
 	{
 		int i, j;

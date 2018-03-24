8a62ec95e1165e3a32c0db44e482c4c4760d33f7
cjwunsch <cwunsch@iastate.edu>
2018-03-23 21:20:41 -0500

    hey

--- src/Piece.java
+++ src/Piece.java
@@ -1,4 +1,60 @@
 
 public abstract class Piece {
 
+	private int currY, currX;
+	private boolean isWhite, wasCaptured, hasMoved;
+	
+	public Piece(int y, int x, boolean isWhite)
+	{
+		currY = y;
+		currX = x;
+		this.isWhite = isWhite;
+		wasCaptured = false;
+		hasMoved = false;
+	}
+	
+	public int getX()
+	{
+		return currX;
+	}
+	
+	public int getY()
+	{
+		return currY;
+	}
+	
+	public void setX(int x)
+	{
+		currX = x;
+	}
+	
+	public void setY(int y)
+	{
+		currY = y;
+	}
+	
+	public boolean isWhite()
+	{
+		return isWhite;
+	}
+	
+	public boolean wasCaptured()
+	{
+		return wasCaptured;
+	}
+	
+	public void captured()
+	{
+		wasCaptured = true;
+	}
+	
+	public boolean hasMoved()
+	{
+		return hasMoved;
+	}
+	
+	public void moved()
+	{
+		hasMoved = true;
+	}
 }

993d6614d8efc9b3d2cbaa78c0df9ef6b63eb8bf
TheAdamBomb98 <adameliasford@gmail.com>
2018-03-23 22:11:39 -0500

    Board started, attempted Pawn

--- src/Board.java
+++ src/Board.java
@@ -1,4 +1,15 @@
 
 public class Board {
-
+	private Piece[][] board = new Piece[8][8];
+	private boolean isWon;
+	
+	public Board() {
+		//Initialize board start
+		isWon = false;
+	}
+	
+	public Piece[][] getBoard() {
+		return board;
+	}
+	
 }
--- src/Pawn.java
+++ src/Pawn.java
@@ -1,4 +1,17 @@
+import java.util.ArrayList;
 
-public class Pawn {
-
+public class Pawn extends Piece {
+	public Pawn( int x, int y, boolean isWhite) {
+		super(x, y, isWhite);
+	}
+	public ArrayList<Move> findValidMoves(Board board){
+		ArrayList<Move> vm = new ArrayList<Move>();
+		int x = this.getX();
+		int y = this.getY();
+		if( this.isWhite() ) {
+			if(board.getBoard()[x][y + 1] == null) {
+				vm.add(new Move(x, y, x, y + 1 , false));
+			}
+		}
+	}
 }

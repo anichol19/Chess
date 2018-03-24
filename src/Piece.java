986757e9a247fe29494edb43e43937c218c88b58
cjwunsch <cwunsch@iastate.edu>
2018-03-23 20:48:40 -0500

    hey

--- src/Piece.java
+++ src/Piece.java
@@ -1,5 +1,25 @@
-
 public abstract class Piece {
-	//comment
-	//can i win?
+	
+	private int currY, currX;
+	private boolean isWhite, isCaptured, hasMoved;
+	
+	public Piece(int y, int x, boolean isWhite, boolean isCaptured)
+	{
+		currY = y;
+		currX = x;
+		this.isWhite = isWhite;
+		this.isCaptured = isCaptured;
+	}
+	
+	public int getX()
+	{
+		return currX;
+	}
+	
+	public int getY()
+	{
+		return currY;
+	}
+	
+	
 }

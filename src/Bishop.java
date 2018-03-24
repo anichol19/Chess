ec64b9082bfa4e42b0333053acc29f7d9dc2a46e
cjwunsch <cwunsch@iastate.edu>
2018-03-24 12:55:19 -0500

    updated Bishop

--- src/Bishop.java
+++ src/Bishop.java
@@ -9,7 +9,65 @@ public class Bishop extends Piece {
 	
 	public ArrayList<Move> findValidMoves(Board board)
 	{
-		ArrayList<Move> possMoves = new ArrayList<Move>();
-		return possMoves;
+		int row1 = getRow(), row2, col1 = getCol(), col2;
+		ArrayList<Move> vm = new ArrayList<Move>();
+		Piece[][] cb = board.getBoard();
+		//upper right
+		row2 = row1 - 1;
+		col2 = col1 + 1;
+		while (cb[row2][col2] == null)
+		{
+			vm.add(new Move(row1, col1, row2, col2, false));
+			row2--;
+			col2++;
+			if (cb[row2][col2] != null && (this.isWhite() != cb[row2][col2].isWhite()))
+				vm.add(new Move(row1, col1, row2, col2, true));
+			else if (cb[row2][col2] != null)
+				break;
+		}
+		//upper left
+		row2 = row1 - 1;
+		col2 = col1 - 1;
+		while (cb[row2][col2] == null)
+		{
+			vm.add(new Move(row1, col1, row2, col2, false));
+			row2--;
+			col2--;
+			if (cb[row2][col2] != null && (this.isWhite() != cb[row2][col2].isWhite()))
+				vm.add(new Move(row1, col1, row2, col2, true));
+			else if (cb[row2][col2] != null)
+				break;
+		}
+		//lower right
+		row2 = row1 + 1;
+		col2 = col1 + 1;
+		while (cb[row2][col2] == null)
+		{
+			vm.add(new Move(row1, col1, row2, col2, false));
+			row2++;
+			col2++;
+			if (cb[row2][col2] != null && (this.isWhite() != cb[row2][col2].isWhite()))
+				vm.add(new Move(row1, col1, row2, col2, true));
+			else if (cb[row2][col2] != null)
+				break;
+		}
+		//lower left
+		row2 = row1 + 1;
+		col2 = col1 - 1;
+		while (cb[row2][col2] == null)
+		{
+			vm.add(new Move(row1, col1, row2, col2, false));
+			row2++;
+			col2--;
+			if (cb[row2][col2] != null && (this.isWhite() != cb[row2][col2].isWhite()))
+				vm.add(new Move(row1, col1, row2, col2, true));
+			else if (cb[row2][col2] != null)
+				break;
+		}
+	}
+	
+	public String toString()
+	{
+		return "B";
 	}
 }
